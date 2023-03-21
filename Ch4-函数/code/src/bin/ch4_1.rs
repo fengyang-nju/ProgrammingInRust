69a609cfe24ca9ee4ae2b1afa78e79b570864124
Xiaolong Fu <75103649+191220029@users.noreply.github.com>
2023-03-21 14:29:19 +0800

    3-21

--- Ch4-函数/code/src/bin/ch4_1.rs
+++ Ch4-函数/code/src/bin/ch4_1.rs
@@ -1,4 +1,4 @@
-//这是一个main函数实例
+//这是一个main函数示例
 fn main() {
     println!("demo that defines a main function");
 }
--- Ch4-函数/code/src/bin/ch4_2.rs
+++ Ch4-函数/code/src/bin/ch4_2.rs
@@ -1,4 +1,4 @@
-//这是一个函数定义的实例
+//这是一个函数定义的示例
 fn plus_one_i32 (param1: i32) -> i32 {
     return param1 + 1;
 }
